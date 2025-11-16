7fbac8010eaa095dcc3d6504c74e316decf59c22
Jefferson Fernandes <suportefinanceiro@rcostaadv.com.br>
2025-11-16 19:15:01 -0300

    Adiciona daphne e channels ao requirements2

--- gestao_casos/settings.py
+++ gestao_casos/settings.py
@@ -121,8 +121,8 @@ CHANNEL_LAYERS = {
     },
 }
 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
